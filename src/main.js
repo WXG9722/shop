b63028d09c15bf6f168826aae8bc87ebede85860
WXG9722 <1321844625@qq.com>
2018-11-23 18:36:08 +0800

    shop

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ Vue.config.productionTip = false
 import '@/assets/css/reset.css'
 import '@/assets/js/rem.js'
 
+import {NavBar, Icon, Tabbar, TabbarItem, Swipe, SwipeItem, Lazyload} from 'vant'
+Vue.use(NavBar).use(Icon).use(Tabbar).use(TabbarItem).use(Swipe).use(SwipeItem).use(Lazyload)
+
 new Vue({
   router,
   store,
