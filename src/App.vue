d9f3d282a2efea8f55b1267b43590ced3d1a4882
WXG9722 <1321844625@qq.com>
2018-11-29 16:34:39 +0800

    shop

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,6 @@
       <van-tabbar-item icon="cart" to="/cart">购物车</van-tabbar-item>
       <van-tabbar-item icon="contact" to="/profile">分类</van-tabbar-item>
     </van-tabbar>
-    <!-- <ul class="footer-bar">
-      <li class="footer-bar-item">
-        <router-link to="/">首页</router-link>
-      </li>
-      <li class="footer-bar-item">
-        <router-link to="/category">分类</router-link>
-      </li>
-      <li class="footer-bar-item">
-        <router-link to="/cart">购物车</router-link>
-      </li>
-      <li class="footer-bar-item">
-        <router-link to="/profile">我的</router-link>
-      </li>
-    </ul> -->
   </div>
 </template>
 
