e1fe22491e80ff4386765bc762477269b61f987d
WXG9722 <1321844625@qq.com>
2019-03-14 15:07:24 +0800

    shop

--- vue.config.js
+++ vue.config.js
@@ -7,13 +7,17 @@ module.exports = {
         open: true
     },
     configureWebpack: config => {
+        // 判断是否是生产环境
         if(process.env.NODE_ENV === 'production'){
             return {
                 plugins: [
+                    // 正则 js html css文件
+                    // 第二个表示超过10kb的进行压缩
+                    // 是否删除源文件
                     new CompressionPlugin({
                         test: /\.js$|\.html$|\.css$/,
                         threshold: 10240,
-                        deleteOriginalAssets: false
+                        deleteOriginalAssets: true
                     })
                 ]
             }
@@ -21,4 +25,4 @@ module.exports = {
     }
 }
 
-// nginx
+// nginx 高性能http和反向代理的服务器 经常用于分布式
